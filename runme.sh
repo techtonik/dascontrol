c30c6bf26d8ffd3504e664f8ad4e17067a130c2c
Ales Guzik <me@aguzik.net>
2013-01-19 20:30:59 +0300

    fix runme, chmod +x, added .gitignore

--- runme.sh
+++ runme.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # [x] Recorder
 # [x] Recognizer
 # [ ] Matcher
@@ -11,7 +12,7 @@ rec -q sample.ogg
 sox sample.ogg -q --rate 16000 -t flac - 2>/dev/null| curl --data-binary @- --header 'Content-type: audio/x-flac; rate=16000' 'https://www.google.com/speech-api/v1/recognize?lang=ru-RU&client=chromium' 2>/dev/null | tee /dev/tty|jshon -e hypotheses -e 0 -e utterance -u > sample.txt
 
 # [ ] Matcher
-
+cp sample.txt action.txt
 
 # [ ] Executor
-echo action.txt
+cat action.txt
