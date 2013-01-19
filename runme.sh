4308345170fe8357c36b6fd0e0dcbc590852bb7c
Ales Guzik <me@aguzik.net>
2013-01-20 00:52:47 +0300

    added matcher and fixed python2

--- runme.sh
+++ runme.sh
@@ -6,13 +6,13 @@
 # [x] Executor
 
 # Recorder
-python recorder.py sample.flac
+python2 recorder.py sample.flac
 
 # Recognizer
 curl --data-binary @sample.flac -k --header "Content-type: audio/x-flac; rate=16000" "https://www.google.com/speech-api/v1/recognize?lang=ru-RU&client=chromium" | tee /dev/tty|jshon -e hypotheses -e 0 -e utterance -u > sample.txt
 
 # [ ] Matcher
-cp sample.txt action.txt
+clisp matcher.lisp
 
 # [x] Executor
-python executor.py action.txt
+python2 executor.py action.txt
